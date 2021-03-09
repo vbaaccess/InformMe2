d8357181e321b9629bcf6546578892fed3ac7e8f
koteR <42820377+vbaaccess@users.noreply.github.com>
2021-03-09 17:41:49 +0100

    Databaes structur - tabInformMe

---
+++ DB Structure - InformMe.sql
@@ -0,0 +1,12 @@
+﻿/*
+   Database for project
+*/
+
+CREATE Table tabInformMe
+(
+ Id INT
+,DatValue VARCHAR(254)
+,DatDescription VARCHAR(254)
+,CreatDate DATETIME DEFAULT(GETDATE())
+,CONSTRAINT [PK_tabInformMe] PRIMARY KEY CLUSTERED ([Id]) ON [PRIMARY]
+)
